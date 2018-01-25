4054a59ace56dd00c2748ea478fdbebb68e50e25
Hao Lee <occbuu>
2018-01-25 15:03:39 +0700

    again...

---
+++ src/app/views/components/modals.component.ts
@@ -0,0 +1,15 @@
+import { Component, ViewChild } from '@angular/core';
+
+@Component({
+  templateUrl: 'modals.component.html'
+})
+export class ModalsComponent {
+    public myModal;
+    public largeModal;
+    public smallModal;
+    public primaryModal;
+    public successModal;
+    public warningModal;
+    public dangerModal;
+    public infoModal;
+}
